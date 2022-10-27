2dcf5a1fef7e30105db91fe605d70e0fde08f3b0
Jerome Habanz <jerome_jero@t-online.de>
2022-10-27 16:04:05 +0200

    added STACK AND INSTRUCTIONS with OPCODES

--- aufgaben/a0/njvm.c
+++ aufgaben/a0/njvm.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+#define MAXITEMS 100
 
+// INSTRUCTIONS
+#define HALT 0
+#define PUSHC 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define MOD 6
+#define RDINT 7
+#define WRINT 8
+#define RDCHR 9
+#define WRCHR 10
+
+// CONVERT
+#define IMMEDIATE(x) ((x) & 0x00FFFFFF)
+#define SIGN_EXTEND(i) ((i) & 0x00800000 ? (i) | 0xFF000000 : (i))
+
+
+int sp = 0;
+int stack[MAXITEMS];
+
+unsigned int program_memory[];
+int mp;
+
+
+void push(int x) {
+    stack[sp] = x;
+    sp++;
+}
+
+int pop() {
+    sp--;
+    int tmp = stack[sp];
+    stack[sp] = 0;
+    return tmp;
+}
+
+void print_stack(void) {
+    printf("\n Stack\n");
+    printf(".-------+--------.\n");
+    for (int i=sp; i>=0; i--) {
+        if (i==sp)
+            printf("|sp->%3d| <empty>|\n", i);
+        else
+            printf("|%7d| %5d |\n", i, stack[i]);
+    }
+    printf("'-------+--------'\n\n");
+}
+
+
+int main(int argc, char *argv[]) {
     if(argc == 2) {
         if(strcmp(argv[1], "--help") == 0) {
             printf("usage: ./njvm [option] [option] ...\n");
@@ -19,6 +70,9 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Ninja Virtual Machine started\n");
+
+    printf("%d\n", IMMEDIATE(0x00FFFFFF));
+
     printf("Ninja Virtual Machine stopped\n");
     return 0;
 }
