65017648017805e93757288f9aeee5fcbab39775
Jerome Habanz <jerome_jero@t-online.de>
2022-11-25 11:58:58 +0100

    tests all .bin files

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
-gcc -g -Wall -std=c99 -pedantic -o vm njvm.c \
-&& \
-./vm testfiles/bin/prog1.bin
+gcc -g -Wall -std=c99 -pedantic -o vm njvm.c;
+for t in testfiles/bin/*.bin; do
+  echo "TESTING: $t";
+  ./vm $t > o1;
+  ./testfiles/njvm $t > o2;
+  DIFF=$(diff o1 o2);
+  if ["$DIFF" == ""]; then
+    echo "PASSED";
+  fi
+done;
