5aadcbb3dda7e364fc5ebdf3644fb5f49ea5548d
jerome <jerome_jero@t-online.de>
2022-11-29 09:16:41 +0100

    minor changes

--- shell/run.sh
+++ shell/run.sh
@@ -1,7 +1,9 @@
 gcc -g -Wall -std=c99 -pedantic -o vm njvm.c;
 for t in testfiles/bin/*.bin; do
   echo "TESTING: $t";
+  echo "NJVM:";
   ./vm $t > o1;
+  echo "REF:";
   ./testfiles/njvm $t > o2;
   DIFF=$(diff o1 o2);
   if [ "$DIFF" == "" ]; then
